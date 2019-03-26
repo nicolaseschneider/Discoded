61704e7a8e8de8296e133aa5ecadbdf3a3236c4d
nicolaseschneider <nicolas.e.schneider@vanderbilt.edu>
2019-03-26 10:47:03 -0400

    complete package.json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user, :logged_in?:action_controller
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+  def logged_in?
+    !!current_user
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+  def login(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+  end
+
 end
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,3 +1,17 @@
-import React from 'react';
 import { createStore } from 'react-redux';
 
+const thunk =( {getstate, dispatch} ) => next => action => {
+  if(action instanceof Function){
+    return action(dispatch, getState);
+  }
+  return next(action);
+};
+
+export default (preloadedState = {}) => (
+  createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(thunk)
+  )
+);
+ 
