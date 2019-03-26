85b6a344daa3532e68da110f84c23de90e11df75
nicolaseschneider <nicolas.e.schneider@vanderbilt.edu>
2019-03-26 10:26:11 -0400

    complete session controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,3 +1,21 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user.nil?
+      render json: ["Invalid username/password"], status: 401
+    else
+      login_user(@user)
+      render :show
+    end
+  end
+
+  def destroy
+    if current_user
+      logout!
+      render json: {}
+    else
+      render status_code: 404
+    end
+  end
 
 end
