b1f31489db184f8ab07341f9be762306cafa38d5
nicolaseschneider <nicolas.e.schneider@vanderbilt.edu>
2019-03-26 17:10:16 -0400

    pushing

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -9,7 +9,7 @@ const App = (props) => (
     <header>
       <h1> Discoded </h1>
     </header>
-    {/* <SuperSplashPage currentUser={props.store.session.currentUser} /> */}
+    {/* <SuperSplashPage currentUser={props.session.currentUser} /> */}
     <AuthRouteLoggedOut path="/login" component={LoginFormContainer} />
     <AuthRouteLoggedOut path="/signup" component={SignUpFormContainer} />
 
