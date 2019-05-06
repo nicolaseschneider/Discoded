eaabe262b79a161f389739c2a583dd52db17ce6f
NicolasESchneider <nicolas.e.schneider@vanderbilt.edu>
2019-05-06 18:18:41 -0400

    turn is the way

--- frontend/reducers/entities/channels_reducer.js
+++ frontend/reducers/entities/channels_reducer.js
@@ -6,10 +6,6 @@ import {RECEIVE_USERS } from '../../actions/user_actions';
 export default (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
-    case RECEIVE_USERS:
-      let newState = merge({},state,{});
-      newState[action.payload.channel_id].users = Object.keys(action.payload.users) 
-      return newState
     case RECEIVE_ALL_CHANNELS:
       return action.channels;
     case RECEIVE_CHANNEL:
