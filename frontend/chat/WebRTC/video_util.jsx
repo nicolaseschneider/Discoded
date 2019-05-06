9ea4af89c8d9caf9248f424b9f4fa3d1f93d2c0d
NicolasESchneider <nicolas.e.schneider@vanderbilt.edu>
2019-05-06 18:15:31 -0400

    turn is the way

--- frontend/chat/WebRTC/video_util.jsx
+++ frontend/chat/WebRTC/video_util.jsx
@@ -6,7 +6,7 @@ export const LEAVE_CALL = "LEAVE_CALL";
 //get the local video stream
 // Ice Credentials
 export const ice = { 
-    iceServers: [{ urls: ["stun:u3.xirsys.com"] }, { username: "53grIOQ2r6pyr79jObt9ha9KIfI4cPjpsxgVEOQYIfCNo6tRVum70_RL2e5gTPkWAAAAAFzCGRdEaW5nb01hbjg5", credential: "15769ebc-6799-11e9-a1f7-86b7e87eee77", urls: ["turn:u3.xirsys.com:80?transport=udp", "turn:u3.xirsys.com:3478?transport=udp", "turn:u3.xirsys.com:80?transport=tcp", "turn:u3.xirsys.com:3478?transport=tcp", "turns:u3.xirsys.com:443?transport=tcp", "turns:u3.xirsys.com:5349?transport=tcp"] }]
+    iceServers: [{ username: "53grIOQ2r6pyr79jObt9ha9KIfI4cPjpsxgVEOQYIfCNo6tRVum70_RL2e5gTPkWAAAAAFzCGRdEaW5nb01hbjg5", credential: "15769ebc-6799-11e9-a1f7-86b7e87eee77", urls: ["turn:u3.xirsys.com:80?transport=udp", "turn:u3.xirsys.com:3478?transport=udp", "turn:u3.xirsys.com:80?transport=tcp", "turn:u3.xirsys.com:3478?transport=tcp", "turns:u3.xirsys.com:443?transport=tcp", "turns:u3.xirsys.com:5349?transport=tcp"] }]
 }
 export const broadcastData = data => {
     fetch("calls", {
